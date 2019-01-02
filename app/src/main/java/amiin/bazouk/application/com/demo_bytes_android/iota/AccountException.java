f93b6408a31b349ab5418d9b5d4d0ad368a42f75
phani <pasupulaphani@gmail.com>
2019-01-02 11:58:49 +0000

    Added crash analytics

--- app/src/main/java/amiin/bazouk/application/com/demo_bytes_android/iota/AccountException.java
+++ app/src/main/java/amiin/bazouk/application/com/demo_bytes_android/iota/AccountException.java
@@ -1,11 +1,10 @@
 package amiin.bazouk.application.com.demo_bytes_android.iota;
 
-public class AccountException extends Exception {
-    public AccountException(String message) {
-        super(message);
-    }
+import com.crashlytics.android.Crashlytics;
 
+public class AccountException extends Exception {
     public AccountException(String message, Throwable cause) {
         super(message, cause);
+        Crashlytics.logException(cause);
     }
 }
